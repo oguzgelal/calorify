13a145209670dd43e8f4d5ad05dd3f58e24bced9
oguzgelal <o.gelal77@gmail.com>
2016-10-25 15:12:26 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# calorify
+
+a [Sails](http://sailsjs.org) application
---
+++ config/seeds.js
@@ -0,0 +1,33 @@
+/**
+ * Sails Seed Settings
+ * (sails.config.seeds)
+ *
+ * Configuration for the data seeding in Sails.
+ *
+ * For more information on configuration, check out:
+ * http://github.com/frostme/sails-seed
+ */
+module.exports.seeds = {
+  user: [
+    {
+      name: 'Oguz Gelal',
+      school: 'VUB'
+    },
+    {
+      name: 'Subhani Shaik',
+      school: 'VUB'
+    },
+    {
+      name: 'Cristina Merino',
+      school: 'VUB'
+    },
+    {
+      name: 'Oscar Corredor',
+      school: 'VUB'
+    },
+    {
+      name: 'Jinjiu Liu',
+      school: 'VUB'
+    },
+  ]
+}
